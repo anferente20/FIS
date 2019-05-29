b73294002be694ed830b399121cbc90108f1b7bb
Andres Arias <leonardoandres.arias09@gmail.com>
2019-05-29 08:41:07 -0500

    Diagrama de estados para clase Reserva

---
+++ Modelos/estados/Agua.java
@@ -0,0 +1,13 @@
+public class Agua{
+private int temp;
+private Estado estado;
+private enum estados{Liquido,Solido,Gaseoso};
+private estados estadoActual=estados.Liquido;
+public void cambiarTemp(){}
+private void fusion(){}
+private void solidificacion(){}
+private void vaporizacion(){}
+private void condensacion(){}
+private void sublimacion(){}
+public void sublimacionInversa(){}
+}
