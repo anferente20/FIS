a0a03346fb125907cd719e94412382be7ceebed2
anferente20 <34009575+anferente20@users.noreply.github.com>
2019-06-28 14:33:19 -0500

    Registros Tipo Suscripcion

---
+++ registros/tipoSuscripcion.sql
@@ -0,0 +1,4 @@
+insert into tiposuscripcion (ipsuscripcion,nombre,descripcion)
+values(1,'Basico','No tiene beneficios. No tiene ningún costo'),
+(2,'Platino','5% de descuento en compras tiene un precio de $50.000 anuales'),
+(3,'Premium','10% de descuento en compras, tiene un precio de $100.000 anuales');
